9945cab2cc0c2476f1b3cd191acfc8f08ecc118f
Carla Allende <106412215+carla-allende@users.noreply.github.com>
2022-10-15 18:17:09 -0300

    Add files via upload
    prácticas :)

---
+++ Clase1.py
@@ -0,0 +1,101 @@
+#LABO 1 EJERCICIO:
+a = 5
+b = 7
+c = 10
+resultado = a + b + c
+print("a:")
+print(a)
+print("b:")
+print(b)
+print("c:")
+print(c)
+print ("resultado:")
+print (resultado)
+
+# de esta forma comento :)
+''' Comentario de mas de una linea '''
+
+#Tipos de datos y variables en Python
+#Tipos numericos
+# Entero (int) var es de tipo entero
+
+var = 10 
+var2 = -6
+
+#Decimales, Reales, punto flotante (float)
+
+res = 3.1415
+print (type(var2))
+
+#Cadena de caracteres (String)
+
+msg = 'Clase 1'
+msg2 = "Python"
+var = "10"
+
+#Logicas (bool) toman dos valores o v o f
+activo = True
+estado = False
+
+#operaciones var numericas
+#Suma (+)
+var1 = 10
+var2 = 5
+res = var1 + var2
+print (res)
+
+#Resta (-)
+var1 = 10
+var2 = 5
+res = var1 - var2
+print (res)
+
+#Multiplicacion (*)
+res = var1 * 2
+
+#Exponente (**)
+res = var2 ** 3
+
+#Division (/)
+res = var1 / 2
+print (res)
+
+#Division entera, solo devuelve la parte entera de una division
+res = var2 // 2
+print (res)
+
+#Modulo (%) -> devuelve el resto de una division
+res = var2 % 2
+print (res)
+
+#operaciones con variables (var) del tipo String son la contecaquenacion 
+var = "Bienvenidos"
+var2 = "a la clase de Python"
+res = var + " " + var2 + " clase n :" + "1"
+print(res)
+
+#Multiplicacion de varialbes de tipo string (*)
+res = var * 5
+print(res)
+
+#LABO 2 EJERCICIO:
+var = "Hola"
+var2 = "Carla"
+res = var + " "+ var2
+print (res)
+
+#Ejercicio Promedio
+nota_uno = 10
+nota_dos = 6
+nota_tres = 8
+prom = (nota_uno + nota_dos + nota_tres) /3
+print(prom)
+promedio = float
+#no lo vemos aun
+if (prom >= 6):  
+print("Aprobo")
+else:   
+print("Desaprobo")
+
+#moser.fabio@gmail.com
+
