1e0d348920b833ac6ff5ed95a094e6a052790e0a
Carla Allende <106412215+carla-allende@users.noreply.github.com>
2022-10-22 15:47:32 -0300

    Add files via upload

---
+++ Ejercicios a resolver.py
@@ -0,0 +1,44 @@
+#PARTE UNO
+conejos = 126
+zanahorias = 0
+print(conejos)
+conejos = 150
+zanahorias = conejos 
+print(zanahorias,conejos)
+conejos = 250
+print (zanahorias,conejos)
+
+#PARTE DOS
+
+#suma
+var1 = 3
+var2 = 6
+res = var1 + var2
+print (res)
+
+#resta
+var1 = 5
+var2 = 4
+res = var1 - var2
+print(res)
+
+#multiplicacion
+var1= 6
+var2 = 3
+res= var1 * var2
+print(res)
+
+#division
+res = 8/2
+print(res)
+
+res= 7/2
+print(res)
+
+#PARTE TRES
+animal = "elefante"
+color = "rosa"
+imagina = animal+color
+print(imagina)
+imagina= animal + color
+print(imagina)
